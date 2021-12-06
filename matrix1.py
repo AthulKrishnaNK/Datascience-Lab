5a2f48093124147022ccd23a47d636940979c2b6
potter360 <potter360@users.noreply.github.com>
2021-12-06 11:42:19 +0530

    ok

---
+++ matrix1.py
@@ -0,0 +1,5 @@
+#Write a program to find the maximum and minimum value from a given matrix.
+import numpy as np
+arr = np.array([[1, 2, 3],[4, 5, 6],[7, 8, 9]])
+print("Maximum value= ",np.max(arr))
+print("Minimum value= ",np.min(arr))
